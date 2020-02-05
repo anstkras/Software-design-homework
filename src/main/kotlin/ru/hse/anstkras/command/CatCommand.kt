f7640f321ff856ac579e6696ed412fd40f637cf3
Krasnoriadtseva Anastasiia <anstkras@gmail.com>
2020-02-05 07:47:06 +0300

    add docs to cat command

--- src/main/kotlin/ru/hse/anstkras/command/CatCommand.kt
+++ src/main/kotlin/ru/hse/anstkras/command/CatCommand.kt
@@ -3,6 +3,7 @@ package ru.hse.anstkras.command
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
 
+/** CatCommand implements standard linux cat command */
 class CatCommand : Command {
     override fun execute(inputStreamReader: InputStreamReader, outputStreamWriter: OutputStreamWriter): Int {
         inputStreamReader.forEachLine {
